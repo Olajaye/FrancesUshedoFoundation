d7c258cbeda036b96f78eb3d3de2ed3d1a256fa2
Jayeola Gbolahan <jayeolajeremiah@gmail.com>
2026-04-08 09:56:13 +0100

    tfut

--- src/store/api/publicNewsApi.ts
+++ src/store/api/publicNewsApi.ts
@@ -1,8 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { News } from "./newsApi";
 
-export interface NewsListItem
-  extends Omit<News, "content" | "stats" | "gallery" | "hidden"> {}
+export type NewsListItem = Omit<News, "content" | "stats" | "gallery" | "hidden">;
 
 export interface PaginatedNewsResponse {
   data: NewsListItem[];
